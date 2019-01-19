b59d5d1fe20912bcb8d858973186e9987ab6dfb0
Michael Konviser <misha.konviser@gmail.com>
2019-01-19 21:04:43 +0200

    Critical formatting

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,28 @@
 #include <string>
 #include "debugger.h"
 
-#define CHILD_PROCESS_ID 0
+#define CHILD_PROCESS_ID (0)
 
 int main(int argc, char * argv[]) {
 
-    if( argc < 2){
+    if(argc < 2) {
             std::cout << "Please enter program name" << std::endl;
             return -1;
     }
 
-    char * programName = argv[1];
+    char* programName = argv[1];
     pid_t pid = fork();
 
     if(pid == CHILD_PROCESS_ID){
         ptrace(PTRACE_TRACEME, CHILD_PROCESS_ID, nullptr, nullptr);
         execl(programName, programName, nullptr);
     }
-    else if( pid >= 1){
+    
+    else if (pid >= 1){
         debugger dbg(programName, pid);
-        dbg.run();
+        dbg.run(); // runs the debugger
     }
 
-
     return 0;
+    
 }
